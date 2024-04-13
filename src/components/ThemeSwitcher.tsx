623a70da5ce80fe76b106a69a3f3635ba0c8743d
kou25 <koustav.manna98@gmail.com>
2024-04-13 18:42:38 +0530

    Refactor ThemeSwitcher component to initialize dark mode based on localStorage value

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,12 +2,9 @@ import { useEffect, useState } from "react";
 import { IoIosSunny, IoIosMoon } from "react-icons/io";
 
 export const ThemeSwitcher = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem("darkMode") === "true";
-    setDarkMode(isDarkMode);
-  }, []);
+  const isAlreadyDarkModeEnabled =
+    localStorage.getItem("darkMode") === "true" ? true : false;
+  const [darkMode, setDarkMode] = useState<boolean>(isAlreadyDarkModeEnabled);
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", darkMode);
